8398830da8786109387bcc02e44dff2cc57e5fba
FedeOrefici <federicoaorefici@gmail.com>
2022-07-10 15:40:32 +0200

    actualizacionRepositorioGauchitoGit

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/homeTest.html"));
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
 
 app.get("/login", (req, res) => {
   res.sendFile(path.resolve("./views/login.html"));
 });
+
 app.get("/register", (req, res) => {
   res.sendFile(path.resolve("views/register.html"));
 });
