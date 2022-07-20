87b97a0cce3c7e7a144f44e80a4bc96b2c101150
FedeOrefici <federicoaorefici@gmail.com>
2022-07-20 19:58:29 +0200

    update

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-/* const controlador = require('./public/src/controllers/mainController.js');
- */
-const mainRoutes = require('./routes/mainRoutes')
+const mainRoutes = require("./routes/mainRoutes");
 
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //correcion ruta para css
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //inicio de server
-app.listen(process.env.PORT || 3050, function(){
-  console.log("servidor corriendo en el puerto 3050")
+app.listen(process.env.PORT || 3050, function () {
+  console.log("servidor corriendo en el puerto 3050");
 });
 
-app.use ('/', mainRoutes);
-
-
+app.use("/", mainRoutes);
